8a550a78390449d779e7e79f9a2a6dc2001c510d
Woohyun Kim <r45r54r45@naver.com>
2016-05-31 23:17:37 +0900

    ddd

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'localhost:8000/';
+$config['base_url'] = 'http://cocodingtest1.cafe24.com/';
 // localhost:8000/
 // http://cocodingtest1.cafe24.com/
 /*
