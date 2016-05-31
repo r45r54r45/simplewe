3051df14955ea08b2ac6e08fd69226c108ff1faa
Woohyun Kim <r45r54r45@naver.com>
2016-05-31 23:51:08 +0900

    ddd

--- application/views/page.php
+++ application/views/page.php
@@ -54,8 +54,8 @@
           <div class="col-xs-12">
             <div class="search-result-block"
             style="background-image: url('{{hospital.IMAGE}}'); background-size:cover;"
-          >
-          <?if($login){?>
+            >
+            <?if($login){?>
               <div class="write_review">
                 <button class=" write-review-button" type="button" >WRITE A REVIEW</button>
               </div>
@@ -152,160 +152,163 @@
                   <img class="img-circle img img-responsive" ng-src="{{doctor_area.PROFILE}}"/>
                   <div class="text-center" style="font-weight:600; font-size:45px; color:white;">{{doctor_area.R_A}}</div>
                   <div class="text-center margin:auto auto;" style="margin-top:-10px;font-weight:600; font-size:15px; color:white;">Doctor Rating</div>
-                  <button class=" write-review-button" style="margin-top:10px;" type="button" onclick="$('#review_modal').modal('show')">WRITE A REVIEW</button>
-                  <div class="row visible-xs" style="margin-top:20px;">
-                    <div class="col-xs-12">
-                      <div style="font-weight:600; color:white; font-size:12px;">Overall Rating</div>
-                      <div ng-bind-html="doctor_area.R_1">
-                      </div>
-                    </div>
-                    <div class="col-xs-12">
-                      <div style="font-weight:600; color:white; font-size:12px;">Bedside Manner</div>
-                      <div ng-bind-html="doctor_area.R_2">
-                      </div>
-                    </div>
-                    <div class="col-xs-12">
-                      <div style="font-weight:600; color:white; font-size:12px;">Expertise</div>
-                      <div ng-bind-html="doctor_area.R_3">
-                      </div>
-                    </div>
-
-                  </div>
-                </div>
-                <div class="media-body" style="padding: 20px 10px;">
-                  <div class="search-result-title">{{doctor_area.NAME}}</div>
-                  <div style="font-size: 9px; color:white; margin-top:10px;">
-                    {{doctor_area.DESCRIPTION}}
-                  </div>
-                  <div class="row hidden-xs" style="margin-top:20px;">
-                    <div class="col-xs-4">
-                      <div style="font-weight:600; color:white; font-size:12px;">Overall Rating</div>
-                      <div ng-bind-html="doctor_area.R_1">
-
-                      </div>
-                    </div>
-                    <div class="col-xs-4">
-                      <div style="font-weight:600; color:white; font-size:12px;">Bedside Manner</div>
-                      <div ng-bind-html="doctor_area.R_2">
-                      </div>
-                    </div>
-                    <div class="col-xs-4">
-                      <div style="font-weight:600; color:white; font-size:12px;">Expertise</div>
-                      <div ng-bind-html="doctor_area.R_3">
-
-                      </div>
-                    </div>
-
-                  </div>
-
-                </div>
-              </div>
-
-
-            </div>
-          </div>
-        </div>
-        <?if($login=="true"){?>
-          <div class="row" ng-show="doctor_info">
-            <div class="col-xs-12">
-              <div class="media" style="background:#595959;margin-bottom:50px;">
-                <div class="media-left hidden-xs"  style="padding: 30px 20px;">
-                  <button class=" write-review-button" style="margin-top:10px; opacity:0" type="button"></button>
-                </div>
-                <div class="media-body"  style="padding: 20px 10px;">
-                  <div class="search-result-title">Reviews</div>
-
-                  <!-- repeat reviews -->
-                  <div class="media" ng-repeat="i in comments" ng-show="$index < commentLimit">
-                    <div class="media-left" style="width: 20%; min-width: 90px;padding: 10px 10px 5px 0px;">
-                      <div class="profile_area">
-                        <img class="img-circle img img-responsive" src="/src/user_profile.jpg"/>
-                      </div>
-                      <div style="margin-top:-5px; font-size:12px;" class="text-center">
-                        <span style="font-weight:600; color:white; font-size:12px;">{{i.NAME}}</span>
-                      </div>
-                      <div style="margin-top:3px; font-size:10px;" class="text-center">
-                        <span style="font-weight:400; color:white; font-size:10px;">{{i.MAJOR}}</span>
-                      </div>
-                    </div>
-                    <div class="media-body" style="padding: 10px 10px; vertical-align: bottom;">
-                      <div style="font-weight:400; color:white; font-size:10px;">{{i.TIME}}</div>
-                      <div style="font-weight:300; color:white; font-size:9px;margin-top:5px;">{{i.BODY}}</div>
-                    </div>
-                  </div>
-
-
-
-
-
-                </div>
-              </div>
-              <div class="row"  ng-show="comments.length>commentLimit">
-                <div class="col-xs-12 text-center" ng-click="loadMore()">
-                  <div class="review-btn-circle">Load More</div>
-                  <div class="review-btn-circle small-review-btn"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <?}?>
-        </div>
-      </div>
-      <!-- modal for review -->
-      <div class="modal fade" id="review_modal" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true" ng-init="review={};">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-body">
-              <div class="row">
-                <div class="col-xs-12" style="padding: 20px 20px 0px; font-size: 18px; font-weight: 500;">
-                  <div class="">
-                    Review for {{doctor_area.NAME}}
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="media">
-                  <div class="media-body" style="padding: 20px;">
-                    <div>
-                      <span class="review_label major_click">Dermatology</span>
-                      <span class=" major_click review_label">Eyeplasty</span>
-                      <span class="major_click review_label">Rhinoplasty</span>
-                      <span class="major_click review_label">Face</span>
-                      <span class="major_click review_label">Breast</span>
-                      <span class="major_click review_label">Body Contour</span>
-                      <span class="major_click review_label">Hair Implant</span>
-                      <span class="major_click review_label">Dentistry</span>
-                      <br/>
-                      <span class="major_click review_label">Medical Examination</span>
-                      <span class="major_click review_label">Other</span>
-                    </div>
-                    <div>
-                      <textarea class="placeholder form-control" rows="9" id="review_body" style="margin-top: 15px; border: 1px solid #494949; box-shadow: none; box-shadow:none;" placeholder="Please tell us about your experience" ng-model="review.body"></textarea>
-                    </div>
-                  </div>
-                  <div class="media-right" style="padding: 10px 10px;  width: 25%;  min-width: 120px;">
-                    <div class="row" style="">
-                      <div class="col-xs-12" style="margin-bottom:10px;">
-                        <div style="font-weight:600; color:#494949; font-size:12px;">Overall Rating</div>
-                        <div >
-                          <input type="number" name="your_awesome_parameter" id="some_id1" ng-model="review.R1" class="rating"  data-icon-lib="fa" data-active-icon="fa-circle" data-inactive-icon="fa-circle-o" />
-                        </div>
-                      </div>
-                      <div class="col-xs-12" style="margin-bottom:10px;">
-                        <div style="font-weight:600; color:#494949; font-size:12px;">Bedside Manner</div>
-                        <div >
-                          <input type="number" name="your_awesome_parameter" id="some_id2" ng-model="review.R2" class="rating"  data-icon-lib="fa" data-active-icon="fa-circle" data-inactive-icon="fa-circle-o" />
+                  <?if($login){?>
+                    <button class=" write-review-button" style="margin-top:10px;" type="button" onclick="$('#review_modal').modal('show')">WRITE A REVIEW</button>
+                    <?}?>
+                    <div class="row visible-xs" style="margin-top:20px;">
+                      <div class="col-xs-12">
+                        <div style="font-weight:600; color:white; font-size:12px;">Overall Rating</div>
+                        <div ng-bind-html="doctor_area.R_1">
                         </div>
                       </div>
                       <div class="col-xs-12">
-                        <div style="font-weight:600; color:#494949; font-size:12px;">Expertise</div>
-                        <div >
-                          <input type="number" name="your_awesome_parameter" id="some_id3" ng-model="review.R3" class="rating"  data-icon-lib="fa" data-active-icon="fa-circle" data-inactive-icon="fa-circle-o" />
+                        <div style="font-weight:600; color:white; font-size:12px;">Bedside Manner</div>
+                        <div ng-bind-html="doctor_area.R_2">
                         </div>
                       </div>
+                      <div class="col-xs-12">
+                        <div style="font-weight:600; color:white; font-size:12px;">Expertise</div>
+                        <div ng-bind-html="doctor_area.R_3">
+                        </div>
+                      </div>
+
                     </div>
-                    <div class="row" style="padding-top: 40px;padding-left: 15px;">
-                      <button class=" write-review-button" type="button"  ng-click="reviewForms()">SUBMIT REVIEW</button>
+                  </div>
+                  <div class="media-body" style="padding: 20px 10px;">
+                    <div class="search-result-title">{{doctor_area.NAME}}</div>
+                    <div style="font-size: 9px; color:white; margin-top:10px;">
+                      {{doctor_area.DESCRIPTION}}
+                    </div>
+                    <div class="row hidden-xs" style="margin-top:20px;">
+                      <div class="col-xs-4">
+                        <div style="font-weight:600; color:white; font-size:12px;">Overall Rating</div>
+                        <div ng-bind-html="doctor_area.R_1">
+
+                        </div>
+                      </div>
+                      <div class="col-xs-4">
+                        <div style="font-weight:600; color:white; font-size:12px;">Bedside Manner</div>
+                        <div ng-bind-html="doctor_area.R_2">
+                        </div>
+                      </div>
+                      <div class="col-xs-4">
+                        <div style="font-weight:600; color:white; font-size:12px;">Expertise</div>
+                        <div ng-bind-html="doctor_area.R_3">
+
+                        </div>
+                      </div>
+
+                    </div>
+
+                  </div>
+                </div>
+
+
+              </div>
+            </div>
+          </div>
+          <?if($login=="true"){?>
+            <div class="row" ng-show="doctor_info">
+              <div class="col-xs-12">
+                <div class="media" style="background:#595959;margin-bottom:50px;">
+                  <div class="media-left hidden-xs"  style="padding: 30px 20px;">
+                    <button class=" write-review-button" style="margin-top:10px; opacity:0" type="button"></button>
+                  </div>
+                  <div class="media-body"  style="padding: 20px 10px;">
+                    <div class="search-result-title">Reviews</div>
+
+                    <!-- repeat reviews -->
+                    <div class="media" ng-repeat="i in comments" ng-show="$index < commentLimit">
+                      <div class="media-left" style="width: 20%; min-width: 90px;padding: 10px 10px 5px 0px;">
+                        <div class="profile_area">
+                          <img class="img-circle img img-responsive" src="/src/user_profile.jpg"/>
+                        </div>
+                        <div style="margin-top:-5px; font-size:12px;" class="text-center">
+                          <span style="font-weight:600; color:white; font-size:12px;">{{i.NAME}}</span>
+                        </div>
+                        <div style="margin-top:3px; font-size:10px;" class="text-center">
+                          <span style="font-weight:400; color:white; font-size:10px;">{{i.MAJOR}}</span>
+                        </div>
+                      </div>
+                      <div class="media-body" style="padding: 10px 10px; vertical-align: bottom;">
+                        <div style="font-weight:400; color:white; font-size:10px;">{{i.TIME}}</div>
+                        <div style="font-weight:300; color:white; font-size:9px;margin-top:5px;">{{i.BODY}}</div>
+                      </div>
+                    </div>
+
+
+
+
+
+                  </div>
+                </div>
+                <div class="row"  ng-show="comments.length>commentLimit">
+                  <div class="col-xs-12 text-center" ng-click="loadMore()">
+                    <div class="review-btn-circle">Load More</div>
+                    <div class="review-btn-circle small-review-btn"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <?}?>
+          </div>
+        </div>
+        <!-- modal for review -->
+        <div class="modal fade" id="review_modal" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true" ng-init="review={};">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-body">
+                <div class="row">
+                  <div class="col-xs-12" style="padding: 20px 20px 0px; font-size: 18px; font-weight: 500;">
+                    <div class="">
+                      Review for {{doctor_area.NAME}}
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="media">
+                    <div class="media-body" style="padding: 20px;">
+                      <div>
+                        <span class="review_label major_click">Dermatology</span>
+                        <span class=" major_click review_label">Eyeplasty</span>
+                        <span class="major_click review_label">Rhinoplasty</span>
+                        <span class="major_click review_label">Face</span>
+                        <span class="major_click review_label">Breast</span>
+                        <span class="major_click review_label">Body Contour</span>
+                        <span class="major_click review_label">Hair Implant</span>
+                        <span class="major_click review_label">Dentistry</span>
+                        <br/>
+                        <span class="major_click review_label">Medical Examination</span>
+                        <span class="major_click review_label">Other</span>
+                      </div>
+                      <div>
+                        <textarea class="placeholder form-control" rows="9" id="review_body" style="margin-top: 15px; border: 1px solid #494949; box-shadow: none; box-shadow:none;" placeholder="Please tell us about your experience" ng-model="review.body"></textarea>
+                      </div>
+                    </div>
+                    <div class="media-right" style="padding: 10px 10px;  width: 25%;  min-width: 120px;">
+                      <div class="row" style="">
+                        <div class="col-xs-12" style="margin-bottom:10px;">
+                          <div style="font-weight:600; color:#494949; font-size:12px;">Overall Rating</div>
+                          <div >
+                            <input type="number" name="your_awesome_parameter" id="some_id1" ng-model="review.R1" class="rating"  data-icon-lib="fa" data-active-icon="fa-circle" data-inactive-icon="fa-circle-o" />
+                          </div>
+                        </div>
+                        <div class="col-xs-12" style="margin-bottom:10px;">
+                          <div style="font-weight:600; color:#494949; font-size:12px;">Bedside Manner</div>
+                          <div >
+                            <input type="number" name="your_awesome_parameter" id="some_id2" ng-model="review.R2" class="rating"  data-icon-lib="fa" data-active-icon="fa-circle" data-inactive-icon="fa-circle-o" />
+                          </div>
+                        </div>
+                        <div class="col-xs-12">
+                          <div style="font-weight:600; color:#494949; font-size:12px;">Expertise</div>
+                          <div >
+                            <input type="number" name="your_awesome_parameter" id="some_id3" ng-model="review.R3" class="rating"  data-icon-lib="fa" data-active-icon="fa-circle" data-inactive-icon="fa-circle-o" />
+                          </div>
+                        </div>
+                      </div>
+                      <div class="row" style="padding-top: 40px;padding-left: 15px;">
+                        <button class=" write-review-button" type="button"  ng-click="reviewForms()">SUBMIT REVIEW</button>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -315,4 +318,3 @@
         </div>
       </div>
     </div>
-  </div>
