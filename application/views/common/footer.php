8e8e26dc3f1e5b37c49a652278de12c88ac65ca2
Woohyun Kim <r45r54r45@naver.com>
2016-06-05 23:05:37 +0900

    fqww

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -7,30 +7,30 @@
   </div>
   <div class="row logo">
     <div class="col-sm-2 col-xs-4 text-center">
-      <img src="/src/canon.png" class="img-responsive">
+      <img src="/src/canon.png" class="hand img-responsive">
     </div>
     <div class="col-sm-2 col-xs-4 text-center">
-      <img src="/src/canon.png" class="img-responsive">
+      <img src="/src/canon.png" class="hand img-responsive">
     </div>
     <div class="col-sm-2 col-xs-4 text-center">
-      <img src="/src/canon.png" class="img-responsive">
+      <img src="/src/canon.png" class="hand img-responsive">
     </div>
     <div class="col-sm-2 col-xs-4 text-center">
-      <img src="/src/canon.png" class="img-responsive">
+      <img src="/src/canon.png" class="hand img-responsive">
     </div>
     <div class="col-sm-2 col-xs-4 text-center">
-      <img src="/src/canon.png" class="img-responsive">
+      <img src="/src/canon.png" class="hand img-responsive">
     </div>
     <div class="col-sm-2 col-xs-4 text-center">
-      <img src="/src/canon.png" class="img-responsive">
+      <img src="/src/canon.png" class="hand img-responsive">
     </div>
   </div>
   <div class="row" style="margin-top:40px;">
     <div class="col-xs-12 ">
       <div class="text-center">
-        <img src="/src/facebookRound.svg" style="width:40px; height:40px; margin-right:17px;">
-        <img src="/src/instagramRound.svg" style="width:40px; height:40px; margin-right:17px;">
-        <img src="/src/youtubeRound.svg" style="width:40px; height:40px;">
+        <img src="/src/facebookRound.svg" style="width:40px; height:40px; margin-right:17px;"  class="hand">
+        <img src="/src/instagramRound.svg" style="width:40px; height:40px; margin-right:17px;"  class="hand">
+        <img src="/src/youtubeRound.svg" style="width:40px; height:40px;"  class="hand">
       </div>
       <div class="text-center" style="margin-top:10px;">
         <span style="color:#eaeaea;font-size:10px;">© simplewe 2016. All Rights Reserved.</span>
